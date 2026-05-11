3f40fc9949ba4c28a570d723440c65eeac2d41d3
Akrom2308 <komiljonovakrom08@gmail.com>
2026-05-12 01:08:09 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -8,10 +8,12 @@ CORS(app)
 
 @app.route("/analyze", methods=["POST"])
 def analyze_ecg():
+
     if "file" not in request.files:
-        return jsonify({"error": "No file uploaded"}), 400
+        return jsonify({"error":"No file uploaded"}), 400
 
     file = request.files["file"]
+
     image = Image.open(file)
 
     rhythms = [
@@ -21,18 +23,14 @@ def analyze_ecg():
     ]
 
     result = {
-        "interpretation": "AI preliminary ECG screening completed.",
+        "interpretation":"AI preliminary ECG screening completed.",
         "rhythm": random.choice(rhythms),
-        "heart_rate": random.randint(60, 110),
-        "signal_quality": "Good",
-        "confidence": random.randint(90, 99)
+        "heart_rate": random.randint(60,110),
+        "signal_quality":"Good",
+        "confidence": random.randint(90,99)
     }
 
     return jsonify(result)
 
-
-import os
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
