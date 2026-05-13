1a847cff30a3effaa324edec61a28470bc0db3b3
Akrom2308 <komiljonovakrom08@gmail.com>
2026-05-14 00:01:59 +0500

    Update detect_peaks.py

--- analyzer/detect_peaks.py
+++ analyzer/detect_peaks.py
@@ -1,22 +1,32 @@
-from scipy.signal import find_peaks
 import numpy as np
-
+from scipy.signal import find_peaks
+from scipy.ndimage import gaussian_filter1d
 
 def detect_r_peaks(signal):
 
-    if signal is None:
-        return []
+    # smooth signal
+    signal = gaussian_filter1d(signal, sigma=2)
 
-    normalized = (
-        signal - np.min(signal)
-    ) / (
-        np.max(signal) - np.min(signal) + 1e-6
+    # normalize
+    signal = signal - np.mean(signal)
+
+    # dynamic threshold
+    threshold = np.max(signal) * 0.45
+
+    # detect peaks
+    peaks, properties = find_peaks(
+        signal,
+        distance=40,
+        height=threshold,
+        prominence=np.max(signal) * 0.2
     )
 
-    peaks, _ = find_peaks(
-        normalized,
-        distance=25,
-        prominence=0.3
-    )
+    # remove false peaks
+    filtered_peaks = []
 
-    return peaks.tolist()
+    for peak in peaks:
+
+        if signal[peak] > threshold:
+            filtered_peaks.append(peak)
+
+    return np.array(filtered_peaks)
