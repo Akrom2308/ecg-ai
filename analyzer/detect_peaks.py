c68a40a7d720c776c6726324a84a2475805d0992
Akrom2308 <komiljonovakrom08@gmail.com>
2026-05-13 23:39:11 +0500

    Update detect_peaks.py

--- analyzer/detect_peaks.py
+++ analyzer/detect_peaks.py
@@ -1,8 +1,22 @@
-import numpy as np
 from scipy.signal import find_peaks
+import numpy as np
+
 
 def detect_r_peaks(signal):
 
-    peaks, _ = find_peaks(signal, distance=20)
+    if signal is None:
+        return []
 
-    return peaks
+    normalized = (
+        signal - np.min(signal)
+    ) / (
+        np.max(signal) - np.min(signal) + 1e-6
+    )
+
+    peaks, _ = find_peaks(
+        normalized,
+        distance=25,
+        prominence=0.3
+    )
+
+    return peaks.tolist()
